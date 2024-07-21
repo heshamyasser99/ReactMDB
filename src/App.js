b23692a6e7713ad46c9ca7275b92f50b6e851ae5
heshamyasser99 <74679502+heshamyasser99@users.noreply.github.com>
2024-07-21 17:54:22 +0300

    First commit

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import "./App.css";
+import "./styles.css";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import MoviesGrid from "./components/MoviesGrid";
+import Watchlist from "./components/Watchlist";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+function App() {
+  return (
+    <div className="App">
+      <div className="container">
+        <Header></Header>
+
+        <Router>
+          <nav></nav>
+
+          <Routes>
+            <Route path="/" element={<MoviesGrid />}></Route>
+            <Route path="/watchlist" element={<Watchlist />}></Route>
+          </Routes>
+        </Router>
+      </div>
+      <Footer></Footer>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "../styles.css";
+
+export default function Header() {
+  return (
+    <div className="header">
+      <img className="logo" src="WidthReactMDB.png" alt="ReactMDB" />
+      <h2>Your favourite movie database!</h2>
+    </div>
+  );
+}
