94f778e4f60797bdc0bb7fdc5ed19af3e3ebf56e
heshamyasser99 <74679502+heshamyasser99@users.noreply.github.com>
2024-07-22 12:28:18 +0300

    Empty Watchlist message

--- src/components/Watchlist.js
+++ src/components/Watchlist.js
@@ -5,21 +5,28 @@ import MovieCard from "./MovieCard";
 export default function Watchlist({ movies, watchlist, toggleWatchlist }) {
   console.log(watchlist);
   return (
-    <div>
-      <h1 className="title">Your Watchlist</h1>
-      <div className="watchlist">
-        {watchlist.map((id) => {
-          const movie = movies.find((movie) => movie.id === id);
-          return (
-            <MovieCard
-              key={id}
-              movie={movie}
-              toggleWatchlist={toggleWatchlist}
-              isWatchlisted={true}
-            />
-          );
-        })}
-      </div>
+    <div className="watchlist-container">
+      <h1>Your Watchlist</h1>
+      {watchlist.length === 0 ? (
+        <div className="empty-watchlist-message">
+          <h1>Your watchlist is empty.</h1>
+          <p>Add movies to your watchlist to see them here!</p>
+        </div>
+      ) : (
+        <div className="watchlist">
+          {watchlist.map((id) => {
+            const movie = movies.find((movie) => movie.id === id);
+            return (
+              <MovieCard
+                key={id}
+                movie={movie}
+                toggleWatchlist={toggleWatchlist}
+                isWatchlisted={true}
+              />
+            );
+          })}
+        </div>
+      )}
     </div>
   );
 }
